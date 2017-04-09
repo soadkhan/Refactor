5483f4050c878fdc9b674e4f9c35cc89428a10b0
soadkhan <khancse5914@gmail.com>
2017-04-09 09:24:57 +0600

    calculate

---
+++ assignment.java
@@ -0,0 +1,43 @@
+
+import java.util.Enumeration;
+
+public class Refactoring {
+	private MyCollection orders = new MyCollection();
+	private String name = "Customer name";
+
+	void printOwing()
+	{
+		Enumeration<E> e = orders.elements();
+		double outstanding = 0.0;
+
+		//print banner
+		print_banner();
+
+		// calculate outstanding
+		outstanding = calculate_outstanding(e, outstanding);
+
+		// print details
+
+		System.out.println("name: " + name);
+		System.out.println("amount: " + outstanding);
+
+	}
+
+	private double calculate_outstanding(Enumeration<E> e, double outstanding) {
+		while(e.hasMoreElements())
+		{
+			Order each = (Order) e.nextElement();
+			outstanding += each.getAmount();
+
+		}
+		return outstanding;
+	}
+
+	private void print_banner() {
+		System.out.println("*****************************");
+		System.out.println("****** Customer Owes ********");
+		System.out.println("*****************************");
+	}
+
+
+}
